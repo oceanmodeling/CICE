0f5aae14a7fa407647ef196a9211c4224bcd3dac
Joseph Smith <smithj28@oregonstate.edu>
2025-07-24 01:20:33 -0500

    fixed ice_comp_nuopc.f90 formating

--- cicecore/drivers/nuopc/cmeps/ice_comp_nuopc.F90
+++ cicecore/drivers/nuopc/cmeps/ice_comp_nuopc.F90
@@ -1030,7 +1030,9 @@ contains
     character(char_len_long)   :: restart_date
     character(char_len_long)   :: restart_filename
     logical                    :: isPresent, isSet
+#ifndef CESMCOUPLED
     logical                    :: write_restartfh
+#endif
     character(len=*),parameter :: subname=trim(modName)//':(ModelAdvance) '
     character(char_len_long)   :: msgString
     !--------------------------------
@@ -1186,6 +1188,7 @@ contains
     call is_restart_fh(clock, restartfh_info, write_restartfh)
     if (write_restartfh) force_restart_now = .true.
 #endif
+
     !--------------------------------
     ! Unpack import state
     !--------------------------------
@@ -1300,10 +1303,12 @@ contains
     character(len=256)       :: stop_option    ! Stop option units
     integer                  :: stop_n         ! Number until stop interval
     integer                  :: stop_ymd       ! Stop date (YYYYMMDD)
-    integer                  :: dtime
     type(ESMF_ALARM)         :: stop_alarm
     character(len=128)       :: name
     integer                  :: alarmcount
+#ifndef CESMCOUPLED
+    integer                  :: dtime
+#endif
     character(len=*),parameter :: subname=trim(modName)//':(ModelSetRunClock) '
     !--------------------------------
 
